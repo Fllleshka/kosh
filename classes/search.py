6c792ebda395ed5a9dda8012ac2ef8fad340c966
Fleshka <fleisner.omsk@gmail.com>
2025-04-01 18:45:45 +0600

    Продолжается работа над поиском. Сообщение редактируется только один раз(

--- classes/search.py
+++ classes/search.py
@@ -21,46 +21,79 @@ class searchprofiles:
                 dates = self.database.selectfromdatabase(request_sportsmen)
         return dates
 
+    # Выбираем данные для меню
+    def selectdatesformenu(self, startindex, endindex, dates, images, markup):
+        while startindex < endindex:
+            imageuser = images.startpathprofile + str(dates[startindex][0]) + "/profile.png"
+            url = "tg://user?id=" + str(dates[startindex][0])
+            name = str(dates[startindex][1]) + " " + str(dates[startindex][2])
+            user = telebot.types.InlineKeyboardButton(name, url=url)
+            markup.add(user)
+            startindex += 1
+
+        return markup
+
     # Печать данных
     def printdates(self, argument, bot, images):
 
         # Получаем все записи по нашему запросу
         datesfromdatabase = self.importdatesfromdatabase(argument)
-
+        changemessage = ""
         # Создание меню
         markup = telebot.types.InlineKeyboardMarkup()
 
         # Кнопка связаться
         if len(datesfromdatabase) != 0:
-            for element in datesfromdatabase:
+            # Определяем количество менюшек в нашем списке
+            countmenues = ((len(datesfromdatabase) + 1) // 5) + 1
+            print("Количество менюшек по 5 позиций:", countmenues)
+
+            markup = self.selectdatesformenu(0, 5, datesfromdatabase, images, markup)
+            print("========================")
+            for elem in range(0, countmenues):
+                print(elem * 5, "\t", (elem+1)*5)
+            print("========================")
+
+            '''for element in datesfromdatabase:
                 indexelem = datesfromdatabase.index(element)
                 imageuser = images.startpathprofile + str(datesfromdatabase[indexelem][0]) + "/profile.png"
                 url = "tg://user?id=" + str(datesfromdatabase[indexelem][0])
                 name = str(datesfromdatabase[indexelem][1]) + " " + str(datesfromdatabase[indexelem][1])
                 user = telebot.types.InlineKeyboardButton(name, url = url)
                 print(element, "\t", imageuser)
-                markup.add(user)
+                markup.add(user)'''
 
             # Кнопки навигации
             leftbutton = telebot.types.InlineKeyboardButton("<", callback_data='print.left')
             rightbutton = telebot.types.InlineKeyboardButton(">", callback_data='print.right')
-            centerbutton = telebot.types.InlineKeyboardButton("0/0", callback_data='print.None')
+            textcenterbutton = "1/" + str(countmenues)
+            centerbutton = telebot.types.InlineKeyboardButton(textcenterbutton, callback_data='print.None')
             markup.add(leftbutton, centerbutton, rightbutton)
 
 
+            markup2 = telebot.types.InlineKeyboardMarkup()
+            centerbutton = telebot.types.InlineKeyboardButton("Тестовая кнопка", callback_data='print.None')
+            markup2.add(centerbutton)
 
             changemessage = self.bot.send_message(self.message.chat.id, "Список тренеров", reply_markup=markup).message_id
         else:
             changemessage = self.bot.send_message(self.message.chat.id, "К сожалению нету анкет подходящих вам.\nПопробуйте позже...")
+        print("\t\t\t", changemessage)
 
         @bot.callback_query_handler(func=lambda call:call.data.startswith('print.'))
         def callbackdata(call):
             match (call.data):
                 case "print.left":
-                    editmessage = bot.edit_message_text(chat_id=call.message.chat.id,
+                    editmessage = bot.edit_message_reply_markup(chat_id=call.message.chat.id,
                                                         message_id=changemessage,
-                                          text="Нажми1")
+                                                        reply_markup=markup2)
+                    print(changemessage)
+                    print(editmessage)
                 case "print.right":
-                    editmessage = bot.edit_message_text(chat_id=call.message.chat.id,
-                                          message_id=changemessage,
-                                          text="Нажми2")
+                    editmessage = bot.edit_message_reply_markup(chat_id=call.message.chat.id,
+                                                        message_id=changemessage,
+                                                        reply_markup=markup2)
+                    print(changemessage)
+                    print("===========")
+                    print(editmessage)
+
